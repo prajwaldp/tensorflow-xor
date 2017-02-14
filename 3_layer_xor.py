d7d063ec08e5843237fe398a2ace693c5a97d648
Prajwal Prakash <prajwalpy6@gmail.com>
2017-02-14 17:29:05 +0530

    initial commit

---
+++ 3_layer_xor.py
@@ -0,0 +1,98 @@
+'''
+A simple neural network to solve 3 input XOR
+'''
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+
+X = np.array([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1]
+], "float32")
+
+y = np.array([
+    [0],
+    [1],
+    [1],
+    [0],
+    [1],
+    [0],
+    [0],
+    [1]
+], "int32")
+
+ohe = OneHotEncoder()
+y = ohe.fit_transform(y).toarray()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=1, random_state=42)
+
+
+inp = tf.placeholder(tf.float32, shape=[None, 3])
+expected = tf.placeholder(tf.float32, shape=[None, 2])
+
+
+# layer 1 parameters
+weights1 = tf.Variable(tf.truncated_normal([3, 4], stddev=0.01))
+biases1 = tf.Variable(tf.zeros([4]))
+hidden1 = tf.nn.tanh(tf.matmul(inp, weights1) + biases1)
+
+
+# layer 2 (ouput layer) parameters
+weights2 = tf.Variable(tf.truncated_normal([4, 2], stddev=0.01))
+biases2 = tf.Variable(tf.zeros([2]))
+logits = tf.matmul(hidden1, weights2) + biases2
+
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, expected)
+loss = tf.reduce_mean(cross_entropy)
+
+train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(expected, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+tf.summary.scalar('Cross Entropy Loss', loss)
+tf.summary.scalar('Accuracy', accuracy)
+merged = tf.summary.merge_all()
+writer = tf.summary.FileWriter("./logs/3-layer")
+
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+
+    sess.run(init)
+
+    print("Training")
+
+    for i in range(5000):
+        sess.run(train_step, feed_dict={inp: X_train,
+                                        expected: y_train})
+
+        summary, acc, err = sess.run([merged, accuracy, loss], feed_dict={inp: X_train,
+                                                                          expected: y_train})
+
+        writer.add_summary(summary, i + 1)
+
+        if (i + 1) % 1000 == 0:
+            print("Epoch: {:5d}\tAcc: {:6.2f}%\tErr: {:6.2f}".format(i + 1, acc * 100, err))
+
+
+    print("\nValidation")
+    acc_test = sess.run(accuracy, feed_dict={inp: X_test,
+                                             expected: y_test})
+
+    acc_train = sess.run(accuracy, feed_dict={inp: X_train,
+                                              expected: y_train})
+
+    print("Accuracy on validation data = {:.2f}%".format(acc_test * 100))
+    print("Accuracy on training data = {:.2f}%".format(acc_train * 100))
---
+++ README.md
@@ -0,0 +1,34 @@
+# Tensorflow and XOR
+
+A neural network built using tensorflow to solve XOR.
+
+### Sample Output
+
+```
+$ python 3_layer_xor.py
+Training
+Epoch:  1000    Acc:  85.71%    Err:   0.43
+Epoch:  2000    Acc:  85.71%    Err:   0.21
+Epoch:  3000    Acc: 100.00%    Err:   0.04
+Epoch:  4000    Acc: 100.00%    Err:   0.02
+Epoch:  5000    Acc: 100.00%    Err:   0.01
+
+Validation
+Accuracy on validation data = 0.00%
+Accuracy on training data = 100.00%
+
+```
+
+```
+$ python 2_layer_xor.py
+Training
+Epoch:  1000    Acc:  66.67%    Err:   0.37
+Epoch:  2000    Acc: 100.00%    Err:   0.10
+Epoch:  3000    Acc: 100.00%    Err:   0.03
+Epoch:  4000    Acc: 100.00%    Err:   0.02
+Epoch:  5000    Acc: 100.00%    Err:   0.01
+
+Validation
+Accuracy on validation data = 0.00%
+Accuracy on training data = 100.00%
+```
